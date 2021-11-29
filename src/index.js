9065ec53c80e261e6b9532823e4bfe1133b30b37
Tim Goloshchapov <tim.goloschapov@gmail.com>
2021-11-29 12:49:40 -0600

    guided project

--- src/index.js
+++ src/index.js
@@ -6,42 +6,63 @@
 // A- finding across the entire DOM
 const header = null
 const logoTitle = null
-const firstCard = null
+const firstCard = document.querySelector('.card:nth-of-type(1)');
 // B- finding within one particular element
-const imageFirstCard = null
-const titleFirstCard = null
+const imageFirstCard = firstCard.querySelector('img');
+const titleFirstCard = firstCard.querySelector('h2');
 const subtitleFirstCard = null
 const textFirstCard = null
 // C- traversing with dot notation
-const link1FirstCard = null
-const link2FirstCard = null
+const link1FirstCard = textFirstCard.nextElementSibling;
+const link2FirstCard = link1FirstCard.nextElementSibling;
 
 
 // 👉 2- Finding collections of elements in the DOM
 // A- Find all the anchor tags inside the nav element
+const links = document.querySelectorAll('nav a');
 // B- Loop over the links and console.log their text content
+links.forEach(function(link){
+  console.log(link);
+});
 // C- Turn the collection of links into a real array
+const linksRealArray = Array.from(links);
 // D- Use .filter to find the anchor tag with the textContent of "Home"
+const foundLink = linksRealArray.find(link => link.textContent === 'Home');
 
+
+// === strict equality, no type coersion '1' === 1 NOOOO
+// == loose equality, type coersion '1' == 1 (don't use ==)
 
 // 👉 3- Changing an element's text content
 //  A- Change the cat-related content into dog-related content
+logoTitle.textContent = 'BloomTech Dog'
+titleFirstCard.textContent = 'Bloomtech blog';
 //  B- Have the students research online the difference between textContent and innerText
 
 
 // 👉 4- Changing any property
 //  A- Using dot notation to change a few attributes
+logoTitle.className = 'logo heading banana';
 //  B- Using .setAttribute to change a few attributes
+link2FirstCard.setAttribute('href', 'https://doggoipsum.com/')
+link1FirstCard.setAttribute('href', 'link to a picture here')
 
 
 // 👉 5- Changing the styling of an element
 //  A- By changing the class names on the element with the classList API
+header.classList.add('sky');
+header.classList.remove('sky');
+header.classList.toggle('sky');
 //  B- By manipulating inline styles on the element
-
+header.style.fontSize = '2em';
 
 // 👉 6- Creating new elements from scratch and appending them
 // Create a new link inside the nav for "Blog"
+const blogLink = document.createElement('a');
+blogLink.textContent = 'Blog';
+blogLink.href = '#';
 
+document.querySelector('nav').appendChild(blogLink);
 
 // 👉 7- Making a copy of the card and appending it to the card group
 // DOM nodes can only exist in one spot in the DOM
